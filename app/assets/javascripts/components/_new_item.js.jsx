841f307f07f83232ed56bbf728b5f4308f20e6cf
Don Cooper <don_cooper@hotmail.com>
2017-09-08 14:57:01 -0400

    New items now being added to list and page immediately

--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -1,4 +1,5 @@
 var NewItem= React.createClass({
+
     handleClick() {
         var name    = this.refs.name.value;
         var description = this.refs.description.value;
@@ -6,18 +7,20 @@ var NewItem= React.createClass({
             url: '/api/v1/items',
             type: 'POST',
             data: { item: { name: name, description: description } },
-            success: (response) => {
-                console.log('it worked!', response);
+            success: (item) => {
+                this.props.handleSubmit(item);
             }
         });
     },
+
     render() {
         return (
-            <div>
-                <input ref='name' placeholder='Enter the name of the item' />
-                <input ref='description' placeholder='Enter a description' />
-                <button onClick={this.handleClick}>Submit</button>
-            </div>
+                <div>
+                    <input ref='name' placeholder='Enter the name of the item' />
+                    <input ref='description' placeholder='Enter a description' />
+                    <button onClick={this.handleClick}>Submit</button>
+                </div>
+
         )
     }
 });
